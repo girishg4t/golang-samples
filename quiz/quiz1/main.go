79d3834b203dc4918e793da7fcb07d567b7eec07
girishg4t <talekar.g@gmail.com>
2020-01-08 11:53:49 +0530

    quiz 1 solutions

--- quiz/quiz1/main.go
+++ quiz/quiz1/main.go
@@ -5,20 +5,50 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
+	argsWithProg := os.Args[1:]
+	var limit int = 30
+	inputArgs := strings.Split(argsWithProg[0], "=")
+
+	if i, err := strconv.Atoi(inputArgs[1]); err == nil && inputArgs[0] == string("limit") {
+		limit = i
+	}
+
 	data, err := readCSVfile("./problems.csv")
 	if err != nil {
 		panic(err)
 	}
+	correctAns := getCorrectAnsCount(data, limit)
 
+	fmt.Printf("\nTotal number of questions correct are : %d out off %d\n", correctAns, len(data))
+
+}
+func getCorrectAnsCount(data [][]string, limit int) int {
+	fmt.Println("limit", limit)
+	var correctAnsCount int = 0
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
-
-	fmt.Println(data[0][1])
+	for i := 0; i < len(data); i++ {
+		fmt.Printf("What is %s : ", data[i][0])
+		c1 := make(chan string, 1)
+		go func() {
+			ans, _ := reader.ReadString('\n')
+			c1 <- ans
+		}()
+		select {
+		case res := <-c1:
+			if strings.TrimRight(res, "\n") == data[i][1] {
+				correctAnsCount++
+			}
+		case <-time.After(time.Duration(limit) * time.Second):
+			return correctAnsCount
+		}
+	}
+	return correctAnsCount
 }
 
 func readCSVfile(filepath string) ([][]string, error) {
